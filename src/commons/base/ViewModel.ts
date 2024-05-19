f2a0e412347a84af90e5694650acbc34b41c9f55
Jek08 <jk.pratama86@gmail.com>
2024-05-19 14:58:51 +0700

    initialize state by constructor in ViewModel base

--- src/commons/base/ViewModel.ts
+++ src/commons/base/ViewModel.ts
@@ -1,5 +1,9 @@
 export default abstract class ViewModel<T> {
-  abstract state: T;
+  state: T;
+
+  constructor(state: T) {
+    this.state = state;
+  }
 
   emit: (action: (state: T) => void) => void = (_) => {};
 }
